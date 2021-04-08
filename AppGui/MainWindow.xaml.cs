a188724f5f7d4b5c49c69a3b43a5d9aad6408c63
lenusca <helenamncardoso@gmail.com>
2021-04-08 09:24:25 +0100

    adding

---
+++ AppGui/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using mmisharp;
+using Newtonsoft.Json;
+
+namespace AppGui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MmiCommunication mmiC;
+        private NetflixService service;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            service = new NetflixService();
+
+            mmiC = new MmiCommunication("localhost",8000, "User1", "GUI");
+            mmiC.Message += MmiC_Message;
+            mmiC.Start();
+
+        }
+
+        // write all movies
+        private void writeAllMovies(List<String> all)
+        {
+            String outFile = "<?xml version=\"1.0\"?><grammar xml:lang=\"pt-PT\" version=\"1.0\" xmlns=\"http://www.w3.org/2001/06/grammar\" tag-format= \"semantics/1.0\"><rule id=\"main\" scope=\"public\"><one-of><item><ruleref uri=\"#object\"/> </item></one-of></rule><rule id= \"object\"><one-of><item><tag>out.Categoria=\"MOVIES\"</tag><one-of><item><ruleref uri=\"#movies\"/><tag>out.movies=rules.movies.movies</tag></item></one-of></item></one-of></rule>";
+            outFile += "<rule id=\"movies\"><item repeat=\"1\"><one-of> ";
+            foreach (String item in all)
+            {
+                if (item != String.Empty)
+                {
+                    Regex reg = new Regex("[*'\",_&#^@]");
+                    string temp = reg.Replace(item, " ");
+
+                    outFile += "<item>" + temp + "<tag> out.movies =" + "\"" + temp + "\"" + "</tag></item>";
+                }
+
+            }
+            outFile += "</one-of></item></rule></grammar>";
+            Console.WriteLine("Writing to movies file");
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter((Environment.CurrentDirectory + @"\..\..\..\..\speechModality\speechModality\all.grxml")))
+            {
+                file.WriteLine(outFile);
+            }
+        }
+
+        // write all series
+        private void writeAllSeries(List<String> all)
+        {
+            String outFile = "<?xml version=\"1.0\"?><grammar xml:lang=\"pt-PT\" version=\"1.0\" xmlns=\"http://www.w3.org/2001/06/grammar\" tag-format= \"semantics/1.0\"><rule id=\"main\" scope=\"public\"><one-of><item><ruleref uri=\"#object\"/> </item></one-of></rule><rule id= \"object\"><one-of><item><tag>out.Categoria=\"SERIES\"</tag><one-of><item><ruleref uri=\"#series\"/><tag>out.series=rules.series.series</tag></item></one-of></item></one-of></rule>";
+            outFile += "<rule id=\"series\"><item repeat=\"1\"><one-of> ";
+            foreach (String item in all)
+            {
+                if (item != String.Empty)
+                {
+                    Regex reg = new Regex("[*'\",_&#^@]");
+                    string temp = reg.Replace(item, " ");
+
+                    outFile += "<item>" + temp + "<tag> out.series =" + "\"" + temp + "\"" + "</tag></item>";
+                }
+
+            }
+            outFile += "</one-of></item></rule></grammar>";
+            Console.WriteLine("Writing to series file");
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter((Environment.CurrentDirectory + @"\..\..\..\..\speechModality\speechModality\all.grxml")))
+            {
+                file.WriteLine(outFile);
+            }
+        }
+
+        private void MmiC_Message(object sender, MmiEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+            var doc = XDocument.Parse(e.Message);
+            var com = doc.Descendants("command").FirstOrDefault().Value;
+            dynamic json = JsonConvert.DeserializeObject(com);
+
+            List<String> all;
+            
+            Console.WriteLine((string)json.recognized.ToString());
+            
+            // profile, movie, serie, genero
+            switch ((string)json.recognized[0].ToString())
+            {
+                case "PROFILE":
+                    service.choosePerfil((string)json.recognized[1].ToString());
+                    break;
+                case "NAVBAR":
+                    service.chooseNavBar((string)json.recognized[1].ToString());
+                    // if i said movies, he write all movies in the file
+                    if ((string)json.recognized[1].ToString() == "Filmes")
+                    {
+                        all = service.getAllMoviesSeries();
+                        Console.WriteLine("Total of movies: " + all.Count());
+                        if (all.Count() > 0)
+                        {
+                            writeAllMovies(all);
+                        }
+                    }
+
+                    else if ((string)json.recognized[1].ToString() == "Séries") 
+                    {
+                        all = service.getAllMoviesSeries();
+                        Console.WriteLine("Total of seres: " + all.Count());
+                        if (all.Count() > 0)
+                        {
+                            writeAllSeries(all);
+                        }
+                    }
+                    break;
+                case "MOVIES":
+                    service.watch((string)json.recognized[1].ToString());
+                    break;
+                case "SERIES":
+                    service.watch((string)json.recognized[1].ToString());
+                    break;
+                case "PLAYERCONTROLS":
+                    service.playerControls((string)json.recognized[1].ToString());
+                    break;
+
+
+
+            }
+
+        }
+    }
+}
---
+++ AppGui/NetflixService.cs
@@ -0,0 +1,139 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGui
+{
+    class NetflixService
+    {
+        private String url = "https://www.netflix.com/browse";
+        private IWebDriver driver;
+        private String username, password; // Netflix Credentials
+
+        public NetflixService()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+            login();
+        }
+
+        public void login() 
+        {
+            try {
+                driver.FindElement(By.XPath("//input[@id='id_userLoginId']")).SendKeys(username);
+                driver.FindElement(By.XPath("//input[@id='id_password']")).SendKeys(password);
+                driver.FindElement(By.XPath("//button[@class='btn login-button btn-submit btn-small']")).Click();
+                System.Threading.Thread.Sleep(1000);
+            }
+            catch(InvalidOperationException e)
+            {
+                return;
+            }
+            catch(NoSuchElementException e)
+            {
+                return;
+            }
+            
+        }
+        
+        public List<String> getAllMoviesSeries()
+        {
+            List<String> all;
+            // driver.FindElement(By.XPath("//*[@id='title-card-2-1']/div[1]/a/div[1]/div/p[@class='fallback-text'] and contains(.,'Mundo Jurássico: Reino Caído')]")).Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+            //Console.WriteLine(driver.FindElements(By.XPath("//p[@class=\"fallback-text\"]//text()")).Select(iw => iw.Text));
+
+            all = null;
+            System.Threading.Thread.Sleep(2000);
+
+            wait.Until(drv => driver.FindElements(By.XPath("//p[@class=\"fallback-text\"]")));
+
+            all = new List<string>(driver.FindElements(By.XPath("//p[@class=\"fallback-text\"]")).Select(iw => iw.Text));
+            Console.WriteLine("TOTALLLLL:"+all.Count());
+            return all;
+        }
+
+        public void choosePerfil(String perfilName)
+        {
+            try 
+            {
+                driver.FindElement(By.XPath("//span[contains(., '"+perfilName+"')]")).Click();
+                System.Threading.Thread.Sleep(1000);
+            }
+            catch(InvalidOperationException e) 
+            {
+                return;
+            }
+            catch (NoSuchElementException e)
+            {
+                return;
+            }
+            
+        }
+
+        public void chooseNavBar(String choice) 
+        {
+            try
+            {
+                driver.FindElement(By.XPath("//ul[@class='tabbed-primary-navigation']//li[@class='navigation-tab']/a[contains(., '" + choice +"')]")).Click();
+                System.Threading.Thread.Sleep(1000);
+            }
+            catch (InvalidOperationException e)
+            {
+                return;
+            }
+            catch (NoSuchElementException e)
+            {
+                return;
+            }
+        }
+
+        public void playerControls(String control) 
+        {
+            Console.WriteLine(control);
+            try
+            {
+                driver.FindElement(By.XPath("//button[@aria-label='"+control+"']")).Click();
+                
+                
+                System.Threading.Thread.Sleep(1000);
+            }
+            catch (InvalidOperationException e)
+            {
+                return;
+            }
+            catch (NoSuchElementException e)
+            {
+            }
+        }
+
+        public void watch(String name)
+        {
+            Console.WriteLine("AQUIIIIIIIIIIIIIIIIIIII");
+            Console.WriteLine(name);
+            
+            try
+            {
+                driver.FindElement(By.XPath("//a[contains(., '" + name + "')]")).Click();
+                System.Threading.Thread.Sleep(2000);
+                driver.FindElement(By.XPath("//button[contains(., 'Ver')]")).Click();
+            }
+            catch (InvalidOperationException e)
+            {
+                return;
+            }
+            catch (NoSuchElementException e)
+            {
+                return;
+            }
+        }
+    }
+}
